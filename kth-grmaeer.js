471de39c398c2ca38a0317d3ea919d0819a85627
HIMANSHU RAI <100830983+rais-github@users.noreply.github.com>
2023-09-02 21:49:11 +0530

    Create kth-grmaeer.js
    Observation skills

---
+++ kth-grmaeer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+
+ // space and time are in order of logn
+var kthGrammar = function(n, k) {
+    if (n === 1) {
+        return 0; // Base case: when n is 1, there's only one symbol, which is 0.
+    }
+
+    const mid = Math.pow(2, n - 2); // Calculate the midpoint of the previous row.
+
+    if (k <= mid) {
+        return kthGrammar(n - 1, k); // Recurse on the previous row if k is in the left half.
+    } else {
+        return 1 - kthGrammar(n - 1, k - mid); // Recurse on the previous row with k adjusted for the right half.
+    }
+};
