ebb3c55a8e57da8cfbe9c8d8670c2ed1fe906ae3
HIMANSHU RAI <100830983+rais-github@users.noreply.github.com>
2024-01-01 22:10:23 +0530

    Create hello.ts

---
+++ hello.ts
@@ -0,0 +1,9 @@
+// hello-world.ts
+
+// Define a function that prints "Hello, World!" to the console
+function sayHello(): void {
+    console.log("Hello, World!");
+}
+
+// Call the function to display the message
+sayHello();
