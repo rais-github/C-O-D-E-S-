f3d159a08809109fc3ddb419f42c4faca81c30bb
HIMANSHU RAI <100830983+rais-github@users.noreply.github.com>
2024-03-02 12:32:07 +0530

    Update checkBipartete.java

--- checkBipartete.java
+++ checkBipartete.java
@@ -1,31 +1,32 @@
 class Solution {
-    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>> adj) {
-        int[] color = new int[V];
-        Arrays.fill(color, -1);
-
-        for (int i = 0; i < V; i++) {
-            if (color[i] == -1) {
-                if (!dfs(i, V, adj, color)) {
-                    return false;
-                }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length, m = graph[0].length;
+        int color[] = new int[n];
+        Arrays.fill(color, 0); 
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int neighbor : graph[i]) {
+                list.add(neighbor);
+            }
+            adj.add(list);
+        }
+        for (int i = 0; i < n; i++) {
+            if (color[i] == 0) {
+                if (!dfs(adj, color, i, 0)) return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(int node, int V, ArrayList<ArrayList<Integer>> adj, int[] color) {
-        if (color[node] == -1) {
-            color[node] = 0; // Assign the current node to one of the colors (0 or 1)
+    private boolean dfs(ArrayList<ArrayList<Integer>> adj, int color[], int src, int clr) {
+        if (color[src] != 0) {
+            return color[src] == clr;
         }
-
-        for (int neighbor : adj.get(node)) {
-            if (color[neighbor] == -1) {
-                color[neighbor] = 1 - color[node]; // Assign the opposite color to the neighbor
-                if (!dfs(neighbor, V, adj, color)) {
-                    return false;
-                }
-            } else if (color[neighbor] == color[node]) {
-                return false; // The graph is not bipartite if there is an edge between nodes with the same color
+        color[src] = clr;
+        for (int neighbor : adj.get(src)) {
+            if (!dfs(adj, color, neighbor, 1 - clr)) {
+                return false;
             }
         }
         return true;
